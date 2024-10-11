28357dd8f3ebd3822f197cff24e5f15cda021b09
Rado070199 <rmazur1999@gmail.com>
2024-10-11 19:26:39 +0200

    Dodano api asp.net

---
+++ Cyfron.API/Data/ApplicationDBContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Cyfron.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        // Dodaj tutaj DbSet dla każdej tabeli, np.:
+        public DbSet<User> YourEntities { get; set; }
+    }
+}
