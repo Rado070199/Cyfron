45a831809fa9bc0a543d19f73adcb88787e79d23
MiloszBiniek <milosz.biniek.1995@gmail.com>
2024-10-16 10:16:19 +0200

    Settings

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ function toggleMenu() {
     const menu = document.getElementById("menu");
     menu.style.display = menu.style.display === "block" ? "none" : "block";
 }
+
+function togglMenu() {
+    const menu = document.getElementById("menu");
+    menu.style.display = menu.style.display === "block" ? "none" : "block";
+}
