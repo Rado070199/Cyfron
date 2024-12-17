32f6cbd26fd1b7d216e23f2ea04eac3c4d6e5f1b
MiloszGit <milosz.biniek.1995@gmail.com>
2024-12-17 18:34:20 +0100

    ScrollBar

--- script.js
+++ script.js
@@ -7,3 +7,12 @@ function togglMenu() {
     const menu = document.getElementById("menu");
     menu.style.display = menu.style.display === "block" ? "none" : "block";
 }
+
+/* ScrollBar */
+$(window).scroll(function() {
+    var windowsScroll = $(window).scrolltop();
+    console.log(windowsScroll);
+    var height = $(document).height() - $(window).height()
+    var scrolled = (windowsScroll / height) * 100;
+    $("#scrollBar").css("width", scrolled + "%");
+});
